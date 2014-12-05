c62d32f641232bc499dacb63af003a794c881422
Evgenus <chernyshov.eugene@gmail.com>
2014-12-05 19:15:17 +0200

    serving bundled js with browserify middleware

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+﻿var browserify = require('browserify-middleware');
+var express = require('express');
+import path = require("path");
+
+var app = express();
+
+browserify.settings.development('basedir', __dirname);
+
+app.get('/app.js', browserify(['./lib/app']));
+
+app.use(express.static(path.resolve(process.cwd(), 'static')));
+
+app.listen(3000);
+console.log("http://127.0.0.1:3000");
