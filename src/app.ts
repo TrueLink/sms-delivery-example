e4f3c416a4fea959feace4aa935b9e44ea6a4b2f
Evgenus <chernyshov.eugene@gmail.com>
2014-12-05 19:14:32 +0200

    dummy application

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,42 @@
-import React = require("react");
+﻿import React = require("react");
+import TypedReact = require("typed-react");
+import uuid = require("node-uuid");
+import hub = require("browser-relay-client/lib/hub");
 var RD = React.DOM;
 
-var App = React.createClass({
-    render: function() {
-        return RD.div(null, "a yo");
-    }
-});
+export interface AppProps {
+    hub: hub.HubAPI
+}
 
-document.addEventListener("DOMContentLoaded", function() {
-    React.renderComponent(App(), document.body);
-}, false );
+interface AppState {
+}
+
+class AppClass extends TypedReact.Component<AppProps, AppState> {
+    componentDidMount() {
+    } 
+
+    componentWillUnmount() {
+    }
+
+    render() {
+        var hub = this.props.hub;
+        return RD.div(null,
+            RD.h1(null, hub.guid),
+            hub.connections().map((item) => {
+                return RD.div(null, item.endpoint);
+                })
+            );
+    }
+}
+
+export var App = React.createFactory(TypedReact.createClass<AppProps, AppState>(AppClass));
+
+window.onload = function () {
+    var guid = uuid.v4();
+    var instance = hub.Hub.create(guid);
+    React.render(App({ hub: instance }), document.body);
+};
+
+window.onerror = function () {
+    console.log.call(console, arguments);
+};
