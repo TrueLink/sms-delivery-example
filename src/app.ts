39412b5e2e70b01564c3e5273d141eb973221848
Evgenus <chernyshov.eugene@gmail.com>
2014-12-09 14:49:24 +0200

    react application: state, self messages

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,104 @@
-﻿import React = require("react");
+﻿import react = require("react");
 import TypedReact = require("typed-react");
 import uuid = require("node-uuid");
+import client = require("browser-relay-client");
+import routing = require("browser-relay-client/lib/routing");
 import hub = require("browser-relay-client/lib/hub");
-var RD = React.DOM;
+var RD = react.DOM;
 
 export interface AppProps {
     hub: hub.HubAPI
 }
 
 interface AppState {
+    messages?: string[];
+    contacts?: string[];
 }
 
 class AppClass extends TypedReact.Component<AppProps, AppState> {
+    getInitialState(): AppState {
+        return {
+            messages: [],
+            contacts: []
+        };
+    }
+
+    private _messageReceived(message: string): void {
+        var messages = this.state.messages;
+        messages.push(message);
+        this.setState({ messages: messages });
+    }
+
+    private _routingChanged(table: routing.RoutingTable): void {
+        var nodes = table.children;
+        var hub = this.props.hub;
+        var contacts = this.state.contacts;
+
+        for (var i = 0; i < nodes.length; i++) {
+            var guid = nodes[i];
+            if (guid == hub.guid) continue;
+            contacts.push(guid);
+        }
+
+        this.setState({ contacts: contacts });
+    }
+
+    private _sendMessage(event: React.FormEvent) {
+        var messageInput = <HTMLInputElement>this.refs["chat-message"].getDOMNode();
+        //var destinationInput = <HTMLSelectElement>this.refs[""].getDOMNode();
+        var message: string = messageInput.value;
+        //var destination: string = destinationInput.value;
+        this._messageReceived(message);
+        var form = <HTMLFormElement>event.target;
+        form.reset();
+        event.preventDefault();
+    }
+
     componentDidMount() {
-    } 
+        var hub = this.props.hub;
+        hub.onMessage.on(this._messageReceived, this);
+        hub.onRoutingChanged.on(this._routingChanged, this);
+    }
 
     componentWillUnmount() {
+        var hub = this.props.hub;
+        hub.onMessage.off(this._messageReceived, this);
     }
 
     render() {
         var hub = this.props.hub;
         return RD.div(null,
             RD.h1(null, hub.guid),
+            RD.textarea({
+                id: "log",
+                value: this.state.messages.join("\n"),
+                readOnly: true
+            }),
             hub.connections().map((item) => {
                 return RD.div(null, item.endpoint);
+            }),
+            RD.form({ onSubmit: this._sendMessage },
+                RD.input({
+                    type: "text",
+                    ref: "chat-message"
+                }),
+                RD.input({
+                    type: "submit",
+                    value: "Send"
                 })
+                )
             );
     }
 }
 
-export var App = React.createFactory(TypedReact.createClass<AppProps, AppState>(AppClass));
+export var App = react.createFactory(TypedReact.createClass<AppProps, AppState>(AppClass));
 
-window.onload = function () {
+window.addEventListener("load", function () {
     var guid = uuid.v4();
     var instance = hub.Hub.create(guid);
-    React.render(App({ hub: instance }), document.body);
-};
+    react.render(App({ hub: instance }), document.body);
+});
 
-window.onerror = function () {
+window.addEventListener("error", function () {
     console.log.call(console, arguments);
-};
+});
